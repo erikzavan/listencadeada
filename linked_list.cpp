213e1287461df7bbe6020180b03df55c0a192d5c
Erik Zavan <131304266+erikzavan@users.noreply.github.com>
2025-04-04 17:25:06 -0300

    Create linked_list.cpp

---
+++ linked_list.cpp
@@ -0,0 +1,72 @@
+#include "linked_list.h"
+#include <stdio.h>
+
+LinkedList::LinkedList() {
+    this->head = nullptr;
+}
+
+LinkedList::~LinkedList() {}  
+
+bool LinkedList::push_front(int key){
+    Node* no = new Node{key, nullptr};
+    if(!no){ //verificar se foi criado
+        return false;
+    } 
+    no->next = this->head;
+    this->head = no;
+    return true;
+}
+
+bool LinkedList::pop_front() {
+    return true;
+}
+
+bool LinkedList::push_back(int key) {
+    return true;
+}
+
+bool LinkedList::equals(LinkedList* other) {
+    return false;
+}
+
+int LinkedList::get(int pos) {
+    return 0;
+}
+
+void LinkedList::print() {}
+
+int LinkedList::size() {
+    return 0;
+}
+
+Node* find(int key) {
+    return nullptr;
+}
+
+bool insert_after(int key, Node* pos) {
+    return true;
+}
+
+bool remove_after(Node* pos) {
+    return true;
+}
+
+bool insert(int pos) {
+    return true;
+}
+
+bool remove(int pos) {
+    return true;
+}
+
+bool remove(int key) {
+    return true;
+}
+
+bool pop_back() {
+    return true;
+}
+
+bool empty() {
+    return true;
+}
